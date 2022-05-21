e28c37c018d1edc46f904bd1f7d188bcacf05cf0
小李xl <1911159016@qq.com>
2022-05-21 10:59:18 +0800

    测试导入Tiled地图

---
+++ resource/tile/16x16 dungeon ii wall reconfig v04 spritesheet.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="16x16 dungeon ii wall reconfig v04 spritesheet" tilewidth="16" tileheight="16" tilecount="192" columns="16">
+ <image source="../sprite/package/itch-io-DungeonTileset4/16x16 dungeon ii wall reconfig v04 spritesheet.png" width="256" height="192"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="3">
+   <object id="25" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="138" duration="200"/>
+   <frame tileid="139" duration="200"/>
+   <frame tileid="140" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
