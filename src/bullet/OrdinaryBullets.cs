8ef80591ac3eeae5a9d7512327d2373325d19e57
小李xl <1911159016@qq.com>
2022-06-07 02:19:50 +0800

    子弹改用Ray计算碰撞

--- src/bullet/OrdinaryBullets.cs
+++ src/bullet/OrdinaryBullets.cs
@@ -3,7 +3,7 @@ using Godot;
 /// <summary>
 /// 普通的子弹
 /// </summary>
-public class OrdinaryBullets : KinematicBody2D, IBullet
+public class OrdinaryBullets : RayCast2D, IBullet
 {
     public CampEnum TargetCamp { get; private set; }
 
@@ -24,8 +24,6 @@ public class OrdinaryBullets : KinematicBody2D, IBullet
     private float FlySpeed = 1500;
     //当前子弹已经飞行的距离
     private float CurrFlyDistance = 0;
-    //碰撞数据
-    private KinematicCollision2D CollisionData = null;
 
     //子弹的精灵
     private Sprite BulletSprite;
@@ -42,38 +40,36 @@ public class OrdinaryBullets : KinematicBody2D, IBullet
         BulletSprite = GetNode<Sprite>("Bullet");
         // BulletSprite.Visible = false;
 
-        Scale = new Vector2(FlySpeed / 60 * 1.5f, 1);
-
-        GD.Print("---------------1" + GlobalPosition);
+        Scale = new Vector2(1.8f, 1);
     }
 
     public override void _PhysicsProcess(float delta)
     {
-        GD.Print("---------------2" + GlobalPosition);
 
-        if (CollisionData != null) //碰到物体
+        if (IsColliding())
         {
-            // 销毁
-            QueueFree();
+            var pos = GetCollisionPoint();
+            GlobalPosition = pos;
             Node2D hit = Hit.Instance<Node2D>();
             hit.RotationDegrees = MathUtils.RandRangeInt(0, 360);
-            hit.GlobalPosition = CollisionData.Position;
+            hit.GlobalPosition = pos;
             GetTree().CurrentScene.AddChild(hit);
-            return;
-        }
-
-        // 移动
-        CollisionData = MoveAndCollide(Vector2.Right.Rotated(GlobalRotation) * FlySpeed * delta);
-
-        if (CurrFlyDistance == 0)
-        {
-            BulletSprite.Visible = true;
-        }
-
-        CurrFlyDistance += FlySpeed * delta;
-        if (CurrFlyDistance >= MaxDistance)
-        {
             QueueFree();
+        }
+        else
+        {
+            Position += new Vector2(FlySpeed * delta, 0).Rotated(Rotation);
+
+            if (CurrFlyDistance == 0)
+            {
+                BulletSprite.Visible = true;
+            }
+
+            CurrFlyDistance += FlySpeed * delta;
+            if (CurrFlyDistance >= MaxDistance)
+            {
+                QueueFree();
+            }
         }
     }
 
