81c1b4e1b1a468915443b619dafc83595f38c729
gamemusicstory <gamemusicstory@outlook.com>
2022-07-03 08:24:50 +0800

    音效和声音

--- src/manager/SoundManager.cs
+++ src/manager/SoundManager.cs
@@ -1,6 +1,16 @@
 using Godot;
 
+/// <summary>
+/// 声音管理 背景音乐管理 音效在特定位置 处理
+/// </summary>
 public class SoundManager
 {
+    public static SoundManager Instance { get => SingleTon.singleTon; }
 
+    private static class SingleTon
+    {
+        internal static SoundManager singleTon = new SoundManager();
+    }
+
+    private AudioStreamPlayer audioStreamPlayer = new AudioStreamPlayer();
 }
