b68b68f05948790b53a761da1e9b84ab8a2bc975
小李xl <1911159016@qq.com>
2022-08-25 13:50:13 +0800

    补一部分注释

---
+++ DungeonShooting_Godot/src/framework/Component.cs
@@ -0,0 +1,97 @@
+﻿
+using Godot;
+
+/// <summary>
+/// 组件基类
+/// </summary>
+public abstract class Component<TG> : IComponent<TG> where TG : Node2D
+{
+    public GameObject<TG> GameObject { get; private set; }
+
+    public Vector2 Position
+    {
+        get => GameObject.Position;
+        set => GameObject.Position = value;
+    }
+
+    public Vector2 GlobalPosition
+    {
+        get => GameObject.GlobalPosition;
+        set => GameObject.GlobalPosition = value;
+    }
+    
+    public bool Visible
+    {
+        get => GameObject.Visible;
+        set => GameObject.Visible = value;
+    }
+
+    public bool Enable { get; set; } = true;
+
+    public bool IsDestroyed { get; private set; }
+
+    private bool _isReady = false;
+    
+    public virtual void Ready()
+    {
+    }
+    
+    public virtual void Process(float delta)
+    {
+    }
+
+    public virtual void PhysicsProcess(float delta)
+    {
+    }
+
+    public virtual void OnDestroy()
+    {
+    }
+
+    public virtual void OnMount()
+    {
+    }
+    
+    public virtual void OnUnMount()
+    {
+    }
+
+    public void Destroy()
+    {
+        if (IsDestroyed)
+        {
+            return;
+        }
+
+        IsDestroyed = true;
+        if (GameObject != null)
+        {
+            GameObject.RemoveComponent(this);
+        }
+
+        OnDestroy();
+    }
+
+    internal void _TriggerProcess(float delta)
+    {
+        if (!_isReady)
+        {
+            _isReady = true;
+            Ready();
+        }
+        Process(delta);
+    }
+    internal void _TriggerPhysicsProcess(float delta)
+    {
+        if (!_isReady)
+        {
+            _isReady = true;
+            Ready();
+        }
+        PhysicsProcess(delta);
+    }
+    internal void SetGameObject(GameObject<TG> gameObject)
+    {
+        GameObject = gameObject;
+    }
+}
--- DungeonShooting_Godot/src/framework/IComponent.cs
+++ DungeonShooting_Godot/src/framework/IComponent.cs
@@ -1,18 +1,48 @@
 ﻿
 using Godot;
 
+/// <summary>
+/// 组件接口
+/// </summary>
 public interface IComponent<TN> : IProcess, IDestroy where TN : Node2D
 {
+    /// <summary>
+    /// 该组件绑定的GameObject对象
+    /// </summary>
     GameObject<TN> GameObject { get; }
     
+    /// <summary>
+    /// 该组件所绑定的GameObject的坐标
+    /// </summary>
     Vector2 Position { get; set; }
+    
+    /// <summary>
+    /// 该组件所绑定的GameObject的全局坐标
+    /// </summary>
     Vector2 GlobalPosition { get; set; }
+    
+    /// <summary>
+    /// 该组件所绑定的GameObject的显示状态
+    /// </summary>
     bool Visible { get; set; }
+    
+    /// <summary>
+    /// 是否启用该组件, 如果停用, 则不会调用 Process 和 PhysicsProcess
+    /// </summary>
     bool Enable { get; set; }
 
+    /// <summary>
+    /// 第一次调用 Process 或 PhysicsProcess 之前调用
+    /// </summary>
     void Ready();
-    void OnMount();
-    void OnUnMount();
     
-    void SetGameObject(GameObject<TN> gameObject);
+    /// <summary>
+    /// 当该组件挂载到GameObject上时调用
+    /// </summary>
+    void OnMount();
+    
+    /// <summary>
+    /// 当该组件被取消挂载时调用
+    /// </summary>
+    void OnUnMount();
 }
