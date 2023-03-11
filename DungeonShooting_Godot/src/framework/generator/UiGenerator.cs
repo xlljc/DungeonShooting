14125a25c748cd945bb0343f22a0a3158547471a
lijincheng <1911159016@qq.com>
2023-03-11 12:40:34 +0800

    解析ui层级

--- DungeonShooting_Godot/src/framework/generator/UiGenerator.cs
+++ DungeonShooting_Godot/src/framework/generator/UiGenerator.cs
@@ -1,7 +1,59 @@
 
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Godot;
+
 namespace Generator;
 
 public static class UiGenerator
 {
+    private static int _nodeIndex = 0;
+    
+    public static void GenerateUi(Control control)
+    {
+        _nodeIndex = 0;
+        var uiNode = EachNode(control);
+    }
+
+    private static UiNode EachNode(Control control)
+    {
+        var name = Regex.Replace(control.Name, "[^\\w_]", "");
+        var uiNode = new UiNode(name, "UiNode" + (++_nodeIndex) + "_" + name, control.GetType().FullName);
+
+        var childCount = control.GetChildCount();
+        if (childCount > 0)
+        {
+            for (var i = 0; i < childCount; i++)
+            {
+                var children = control.GetChildOrNull<Control>(i);
+                if (children != null)
+                {
+                    if (uiNode.Children == null)
+                    {
+                        uiNode.Children = new List<UiNode>();
+                    }
+
+                    uiNode.Children.Add(EachNode(children));
+                }
+            }
+        }
+
+        return uiNode;
+    }
+
+    private class UiNode
+    {
+        public string Name;
+        public string ClassName;
+        public string TypeName;
+        public List<UiNode> Children;
+
+        public UiNode(string name, string className, string typeName)
+        {
+            Name = name;
+            ClassName = className;
+            TypeName = typeName;
+        }
+    }
     
 }
--- DungeonShooting_Godot/src/test/TestUi/Test.cs
+++ DungeonShooting_Godot/src/test/TestUi/Test.cs
@@ -2,7 +2,6 @@ using Godot;
 
 namespace UI;
 
-
 /*
  Test
     c1
