7a6d975f892ef084768c45cfffd4d5952595677c
lijincheng <1911159016@qq.com>
2023-04-24 14:35:04 +0800

    小修改

--- DScript/DScript_Test/Unit/Compiler/CompilerTest1.cs
+++ DScript/DScript_Test/Unit/Compiler/CompilerTest1.cs
@@ -14,15 +14,19 @@ public class CompilerTest1 : UnitTest
     [Fact(DisplayName = "Test1, 测试解析词法")]
     public void Test1()
     {
-        var fileName = "Script/Test.ds";
+        var fileName = "Script/example.ds";
         var text = File.ReadAllText(fileName);
-        var tree = new Lexer();
-        tree.FromSource(text);
-        var arr = tree.GetLexerStrings();
-        for (int i = 0; i < arr.Length; i++)
+            var tree = new Lexer();
+        ExecuteTime.Run("执行用时", () =>
         {
-            LogUtils.Log(arr[i].ToString());
-        }
+            tree.FromSource(text);
+            //var arr = tree.GetLexerStrings();
+            // for (int i = 0; i < arr.Length; i++)
+            // {
+            //     LogUtils.Log(arr[i].ToString());
+            // }
+        });
+
     }
     
     [Fact(DisplayName = "Test2, 测试解析语法树")]
