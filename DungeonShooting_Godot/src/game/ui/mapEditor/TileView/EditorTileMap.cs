e2594dacf8e4a6763cdc8a1ebf804a23e57a6c68
小李xl <1911159016@qq.com>
2023-07-12 00:28:08 +0800

    鼠标滚轮放大缩小地图, 鼠标中键拖动地图

--- DungeonShooting_Godot/src/game/ui/mapEditor/TileView/EditorTileMap.cs
+++ DungeonShooting_Godot/src/game/ui/mapEditor/TileView/EditorTileMap.cs
@@ -4,30 +4,57 @@ namespace UI.MapEditor;
 
 public partial class EditorTileMap : TileMap
 {
+    //鼠标坐标
     private Vector2 _mousePosition;
+    //鼠标所在的cell坐标
     private Vector2I _mouseCellPosition;
+    //上一帧鼠标所在的cell坐标
+    private Vector2I _prevMouseCellPosition = new Vector2I(-99999, -99999);
+    //鼠标中建是否按下
+    private bool _isMiddlePressed = false;
+    private Vector2 _moveOffset;
 
     public override void _Process(double delta)
     {
         var position = GetLocalMousePosition();
         _mouseCellPosition = new Vector2I(
-            (int)(position.X / GameConfig.TileCellSize),
-            (int)(position.Y / GameConfig.TileCellSize)
+            Mathf.FloorToInt(position.X / GameConfig.TileCellSize),
+            Mathf.FloorToInt(position.Y / GameConfig.TileCellSize)
         );
         _mousePosition = new Vector2(
             _mouseCellPosition.X * GameConfig.TileCellSize,
             _mouseCellPosition.Y * GameConfig.TileCellSize
         );
 
+        //左键绘制
         if (Input.IsMouseButtonPressed(MouseButton.Left))
         {
-            SetCell(GameConfig.FloorMapLayer, _mouseCellPosition, 0, new Vector2I(0,8));
+            if (_prevMouseCellPosition != _mouseCellPosition)
+            {
+                _prevMouseCellPosition = _mouseCellPosition;
+                SetCell(GameConfig.FloorMapLayer, _mouseCellPosition, 0, new Vector2I(0,8));
+            }
+        }
+        else if (Input.IsMouseButtonPressed(MouseButton.Right)) //右键擦除
+        {
+            if (_prevMouseCellPosition != _mouseCellPosition)
+            {
+                _prevMouseCellPosition = _mouseCellPosition;
+                EraseCell(GameConfig.FloorMapLayer, _mouseCellPosition);
+            }
+        }
+        else if (_isMiddlePressed) //中建移动
+        {
+            //GD.Print("移动...");
+            Position = GetGlobalMousePosition() + _moveOffset;
         }
     }
 
     public override void _Draw()
     {
         DrawRect(new Rect2(_mousePosition, GameConfig.TileCellSize, GameConfig.TileCellSize), Colors.Wheat, false);
+        DrawLine(new Vector2(0, 2000), new Vector2(0, -2000), Colors.Green);
+        DrawLine(new Vector2(2000, 0), new Vector2( -2000, 0), Colors.Red);
     }
 
     public override void _Input(InputEvent @event)
@@ -44,16 +71,27 @@ public partial class EditorTileMap : TileMap
                 //放大
                 Magnify();
             }
+            else if (mouseButton.ButtonIndex == MouseButton.Middle)
+            {
+                _isMiddlePressed = mouseButton.Pressed;
+                if (_isMiddlePressed)
+                {
+                    _moveOffset = Position - mouseButton.GlobalPosition;
+                    GD.Print(_moveOffset);
+                }
+            }
         }
     }
 
     //缩小
     private void Shrink()
     {
+        var pos = GetLocalMousePosition();
         var scale = Scale / 1.1f;
         if (scale.LengthSquared() >= 0.5f)
         {
             Scale = scale;
+            Position += pos * 0.1f * scale;
         }
         else
         {
@@ -63,10 +101,13 @@ public partial class EditorTileMap : TileMap
     //放大
     private void Magnify()
     {
-        var scale = Scale * 1.1f;
+        var pos = GetLocalMousePosition();
+        var prevScale = Scale;
+        var scale = prevScale * 1.1f;
         if (scale.LengthSquared() <= 2000)
         {
             Scale = scale;
+            Position -= pos * 0.1f * prevScale;
         }
         else
         {
