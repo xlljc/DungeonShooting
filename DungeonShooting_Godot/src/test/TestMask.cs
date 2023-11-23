a5908a1e3524c13976f67d7e8f5e627d4e15d1fd
小李xl <1911159016@qq.com>
2023-11-23 21:20:22 +0800

    调研中

--- DungeonShooting_Godot/src/test/TestMask.cs
+++ DungeonShooting_Godot/src/test/TestMask.cs
@@ -1,36 +1,63 @@
 using Godot;
 using System;
+using System.Linq;
+using Godot.Collections;
 
 public partial class TestMask : Node2D
 {
-	private Sprite2D Sprite;
-	private Image _image;
-	private ImageTexture _texture;
+	[Export]
+	public Polygon2D PolygonNode;
 
-	private Image _maskImage;
-	private Grid<int> _grid;
-	
+	public Array<Vector2[]> Result = new Array<Vector2[]>();
 	public override void _Ready()
 	{
-		Sprite = GetNode<Sprite2D>("Sprite2D");
-		var size = DisplayServer.WindowGetSize();
-		_image = Image.Create(size.X, size.Y, false, Image.Format.Rgba8);
-		//_image.Fill(Colors.Black);
-		_texture = ImageTexture.CreateFromImage(_image);
-		Sprite.Texture = _texture;
-		_maskImage = ResourceManager.Load<Texture2D>(ResourcePath.icon_png).GetImage();
-		//Godot.c
-		//_grid.set
+		//Geometry2D.
+		// var point = new Vector2[]
+		// {
+		// 	new Vector2(0, 0),
+		// 	new Vector2(0, 500),
+		// 	new Vector2(500, 500),
+		// 	new Vector2(500, 0)
+		// };
+		// Result = Geometry2D.ClipPolygons(point, new Vector2[]
+		// {
+		// 	new Vector2(20, 20),
+		// 	new Vector2(20, 100),
+		// 	new Vector2(90, 50),
+		// });
+		//PolygonNode.Polygon = clipPolygons[0];
 	}
-
 
 	public override void _Process(double delta)
 	{
-		if (Input.IsMouseButtonPressed(MouseButton.Left))
+		if (Input.IsActionJustPressed("fire"))
 		{
-			var usedRect = _maskImage.GetUsedRect();
-			_image.BlendRect(_maskImage, usedRect, GetLocalMousePosition().AsVector2I() - usedRect.Size / 2);
-			_texture.Update(_image);
+			var position = GetGlobalMousePosition();
+			Result.Add(new Vector2[]
+			{
+				new Vector2(position.X, position.Y),
+				new Vector2(position.X + 50, position.Y),
+				new Vector2(position.X + 50, position.Y + 50),
+				new Vector2(position.X, position.Y + 50),
+			});
+			// Geometry2D.IsPolygonClockwise()
+			// Geometry2D.MergePolygons()
 		}
+		QueueRedraw();
+	}
+
+	public override void _Draw()
+	{
+		if (Result != null)
+		{
+			foreach (var vector2s in Result)
+			{
+				var list = vector2s.ToList();
+				list.Add(vector2s[0]);
+				DrawPolyline(list.ToArray(), Colors.Red);
+			}
+			//DrawColoredPolygon(Result[1], Colors.Red);
+		}
+		
 	}
 }
