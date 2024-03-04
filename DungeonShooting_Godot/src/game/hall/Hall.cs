a0ed8a7c47b60b708e929eac4362a770a8b81f1f
王晗智 <3566166260@qq.com>
2024-03-04 23:42:39 +0800

    01

--- DungeonShooting_Godot/src/game/hall/Hall.cs
+++ DungeonShooting_Godot/src/game/hall/Hall.cs
@@ -1,4 +1,4 @@
-﻿
+
 using Godot;
 
 /// <summary>
@@ -6,17 +6,17 @@ using Godot;
 /// </summary>
 public partial class Hall : World
 {
-    /// <summary>
-    /// 玩家出生标记
-    /// </summary>
-    [Export]
-    public Marker2D BirthMark;
+	/// <summary>
+	/// 玩家出生标记
+	/// </summary>
+	[Export]
+	public Marker2D BirthMark;
 
-    [Export]
-    public Sprite2D BgSprite;
-    
-    /// <summary>
-    /// 房间数据, 该数据时虚拟出来的, 并不是配置文件读取出来的
-    /// </summary>
-    public RoomInfo RoomInfo { get; set; }
+	[Export]
+	public Sprite2D BgSprite;
+	
+	/// <summary>
+	/// 房间数据, 该数据时虚拟出来的, 并不是配置文件读取出来的
+	/// </summary>
+	public RoomInfo RoomInfo { get; set; }
 }
