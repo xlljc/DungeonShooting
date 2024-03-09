1601af60a62d9e8aae2bb9bb3f2daaae5c17d4ce
王晗智 <3566166260@qq.com>
2024-03-09 22:53:17 +0800

    7

--- DungeonShooting_Godot/src/game/activity/currency/Gold.cs
+++ DungeonShooting_Godot/src/game/activity/currency/Gold.cs
@@ -1,4 +1,4 @@
-﻿
+
 using Godot;
 
 /// <summary>
@@ -7,67 +7,67 @@ using Godot;
 [Tool]
 public partial class Gold : ActivityObject, IPoolItem
 {
-    /// <summary>
-    /// 金币数量
-    /// </summary>
-    [Export]
-    public int GoldCount { get; set; } = 1;
-    
-    public bool IsRecycled { get; set; }
-    public string Logotype { get; set; }
-    
-    private float _maxSpeed = 250;
-    private float _speed = 0;
-    private Role _moveTarget;
-    
-    public override void OnInit()
-    {
-        DefaultLayer = RoomLayerEnum.YSortLayer;
-    }
+	/// <summary>
+	/// 金币数量
+	/// </summary>
+	[Export]
+	public int GoldCount { get; set; } = 1;
+	
+	public bool IsRecycled { get; set; }
+	public string Logotype { get; set; }
+	
+	private float _maxSpeed = 250;
+	private float _speed = 0;
+	private Role _moveTarget;
+	
+	public override void OnInit()
+	{
+		DefaultLayer = RoomLayerEnum.YSortLayer;
+	}
 
-    protected override void OnThrowOver()
-    {
-        var current = Player.Current;
-        if (current != null)
-        {
-            this.CallDelay(0.3f, () =>
-            {
-                _moveTarget = current;
-                MoveController.Enable = false;
-            });
-        }
-    }
+	protected override void OnThrowOver()
+	{
+		var current = Player.Current;
+		if (current != null)
+		{
+			this.CallDelay(0.3f, () =>
+			{
+				_moveTarget = current;
+				MoveController.Enable = false;
+			});
+		}
+	}
 
-    protected override void Process(float delta)
-    {
-        if (_moveTarget != null && !_moveTarget.IsDestroyed)
-        {
-            var position = Position;
-            var targetPosition = _moveTarget.Position;
-            if (position.DistanceSquaredTo(targetPosition) < 3 * 3)
-            {
-                _moveTarget.AddGold(GoldCount);
-                ObjectPool.Reclaim(this);
-            }
-            else
-            {
-                _speed = Mathf.MoveToward(_speed, _maxSpeed, _maxSpeed * delta);
-                Position = position.MoveToward(targetPosition, _speed * delta);
-            }
-        }
-    }
-    
-    public void OnReclaim()
-    {
-        GetParent().RemoveChild(this);
-        _moveTarget = null;
-    }
+	protected override void Process(float delta)
+	{
+		if (_moveTarget != null && !_moveTarget.IsDestroyed)
+		{
+			var position = Position;
+			var targetPosition = _moveTarget.Position;
+			if (position.DistanceSquaredTo(targetPosition) < 3 * 3)
+			{
+				_moveTarget.AddGold(GoldCount);
+				ObjectPool.Reclaim(this);
+			}
+			else
+			{
+				_speed = Mathf.MoveToward(_speed, _maxSpeed, _maxSpeed * delta);
+				Position = position.MoveToward(targetPosition, _speed * delta);
+			}
+		}
+	}
+	
+	public void OnReclaim()
+	{
+		GetParent().RemoveChild(this);
+		_moveTarget = null;
+	}
 
-    public void OnLeavePool()
-    {
-        _speed = 0;
-        MoveController.Enable = true;
-        MoveController.ClearForce();
-        MoveController.SetAllVelocity(Vector2.Zero);
-    }
+	public void OnLeavePool()
+	{
+		_speed = 0;
+		MoveController.Enable = true;
+		MoveController.ClearForce();
+		MoveController.SetAllVelocity(Vector2.Zero);
+	}
 }
--- DungeonShooting_Godot/src/game/room/RoomDoor.cs
+++ DungeonShooting_Godot/src/game/room/RoomDoor.cs
@@ -1,4 +1,4 @@
-﻿
+
 using Godot;
 
 /// <summary>
@@ -7,106 +7,106 @@ using Godot;
 [Tool]
 public partial class RoomDoor : ActivityObject
 {
-    /// <summary>
-    /// 门的方向
-    /// </summary>
-    public DoorDirection Direction => _door.Direction;
-    
-    /// <summary>
-    /// 门是否关闭
-    /// </summary>
-    public bool IsClose { get; private set; }
-    
-    private RoomDoorInfo _door;
-    private bool waitDisabledCollision = false;
-    private AnimatedSprite2D _animatedDown;
+	/// <summary>
+	/// 门的方向
+	/// </summary>
+	public DoorDirection Direction => _door.Direction;
+	
+	/// <summary>
+	/// 门是否关闭
+	/// </summary>
+	public bool IsClose { get; private set; }
+	
+	private RoomDoorInfo _door;
+	private bool waitDisabledCollision = false;
+	private AnimatedSprite2D _animatedDown;
 
-    public override void OnInit()
-    {
-        AnimatedSprite.AnimationFinished += OnAnimationFinished;
-    }
+	public override void OnInit()
+	{
+		AnimatedSprite.AnimationFinished += OnAnimationFinished;
+	}
 
-    /// <summary>
-    /// 初始化调用
-    /// </summary>
-    public void Init(RoomDoorInfo doorInfo)
-    {
-        _door = doorInfo;
-        IsClose = false;
-        if (doorInfo.Direction == DoorDirection.E || doorInfo.Direction == DoorDirection.W)
-        {
-            _animatedDown = GetNode<AnimatedSprite2D>("AnimatedSpriteDown");
-        }
+	/// <summary>
+	/// 初始化调用
+	/// </summary>
+	public void Init(RoomDoorInfo doorInfo)
+	{
+		_door = doorInfo;
+		IsClose = false;
+		if (doorInfo.Direction == DoorDirection.E || doorInfo.Direction == DoorDirection.W)
+		{
+			_animatedDown = GetNode<AnimatedSprite2D>("AnimatedSpriteDown");
+		}
 
-        OpenDoorHandler();
-    }
+		OpenDoorHandler();
+	}
 
-    /// <summary>
-    /// 打开当前的门
-    /// </summary>
-    public void OpenDoor()
-    {
-        IsClose = false;
-        //Visible = false;
-        waitDisabledCollision = true;
-        if (AnimatedSprite.SpriteFrames.HasAnimation(AnimatorNames.OpenDoor))
-        {
-            AnimatedSprite.Play(AnimatorNames.OpenDoor);
-        }
+	/// <summary>
+	/// 打开当前的门
+	/// </summary>
+	public void OpenDoor()
+	{
+		IsClose = false;
+		//Visible = false;
+		waitDisabledCollision = true;
+		if (AnimatedSprite.SpriteFrames.HasAnimation(AnimatorNames.OpenDoor))
+		{
+			AnimatedSprite.Play(AnimatorNames.OpenDoor);
+		}
 
-        if (_animatedDown != null && _animatedDown.SpriteFrames.HasAnimation(AnimatorNames.OpenDoor))
-        {
-            _animatedDown.Play(AnimatorNames.OpenDoor);
-        }
-    }
+		if (_animatedDown != null && _animatedDown.SpriteFrames.HasAnimation(AnimatorNames.OpenDoor))
+		{
+			_animatedDown.Play(AnimatorNames.OpenDoor);
+		}
+	}
 
-    /// <summary>
-    /// 关闭当前的门
-    /// </summary>
-    public void CloseDoor()
-    {
-        IsClose = true;
-        //Visible = true;
-        Collision.Disabled = false;
+	/// <summary>
+	/// 关闭当前的门
+	/// </summary>
+	public void CloseDoor()
+	{
+		IsClose = true;
+		//Visible = true;
+		Collision.Disabled = false;
 
-        if (AnimatedSprite.SpriteFrames.HasAnimation(AnimatorNames.CloseDoor))
-        {
-            AnimatedSprite.Play(AnimatorNames.CloseDoor);
-        }
+		if (AnimatedSprite.SpriteFrames.HasAnimation(AnimatorNames.CloseDoor))
+		{
+			AnimatedSprite.Play(AnimatorNames.CloseDoor);
+		}
 
-        if (_animatedDown != null)
-        {
-            _animatedDown.Visible = false;
-        }
+		if (_animatedDown != null)
+		{
+			_animatedDown.Visible = false;
+		}
 
-        if (Direction == DoorDirection.E || Direction == DoorDirection.W)
-        {
-            ZIndex = MapLayer.CustomMiddleLayer2;
-        }
-    }
+		if (Direction == DoorDirection.E || Direction == DoorDirection.W)
+		{
+			ZIndex = MapLayer.CustomMiddleLayer2;
+		}
+	}
 
-    private void OnAnimationFinished()
-    {
-        if (!IsClose && waitDisabledCollision) //开门动画播放完成
-        {
-            waitDisabledCollision = false;
-            if (_animatedDown != null)
-            {
-                _animatedDown.Visible = true;
-            }
+	private void OnAnimationFinished()
+	{
+		if (!IsClose && waitDisabledCollision) //开门动画播放完成
+		{
+			waitDisabledCollision = false;
+			if (_animatedDown != null)
+			{
+				_animatedDown.Visible = true;
+			}
 
-            if (Direction == DoorDirection.E || Direction == DoorDirection.W)
-            {
-                ZIndex = 0;
-            }
-            OpenDoorHandler();
-        }
-    }
+			if (Direction == DoorDirection.E || Direction == DoorDirection.W)
+			{
+				ZIndex = 0;
+			}
+			OpenDoorHandler();
+		}
+	}
 
-    private void OpenDoorHandler()
-    {
-        Collision.Disabled = true;
-        //调整门的层级
-        //ZIndex = MapLayer.AutoFloorLayer;
-    }
+	private void OpenDoorHandler()
+	{
+		Collision.Disabled = true;
+		//调整门的层级
+		//ZIndex = MapLayer.AutoFloorLayer;
+	}
 }
